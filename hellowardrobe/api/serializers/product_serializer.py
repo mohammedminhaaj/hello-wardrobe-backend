d7df8fbf9ace45be9dc0056ea84293f5c4cae379
Mohammed Minhaaj <mohammedminhaaj97@gmail.com>
2022-11-14 23:02:44 +0530

    Adding pagination

--- hellowardrobe/api/serializers/product_serializer.py
+++ hellowardrobe/api/serializers/product_serializer.py
@@ -3,15 +3,25 @@ from product.models import Product, PrimaryCategory, SecondaryCategory, Size, Ta
 
 
 class PrimaryCategorySerializer(serializers.ModelSerializer):
+    category = serializers.SerializerMethodField()
+
+    def get_category(self, obj):
+        return 'primary'
+
     class Meta:
         model = PrimaryCategory
-        fields = ['name']
+        fields = ['id', 'name', 'category']
 
 
 class SecondaryCategorySerializer(serializers.ModelSerializer):
+    category = serializers.SerializerMethodField()
+
+    def get_category(self, obj):
+        return 'secondary'
+
     class Meta:
         model = SecondaryCategory
-        fields = ['name']
+        fields = ['id', 'name', 'category']
 
 
 class SizeSerializer(serializers.ModelSerializer):
@@ -22,7 +32,7 @@ class SizeSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Size
-        fields = ['id', 'name','category']
+        fields = ['id', 'name', 'category']
 
 
 class TagSerializer(serializers.ModelSerializer):
@@ -33,12 +43,11 @@ class TagSerializer(serializers.ModelSerializer):
 
 class ProductSerializer(serializers.ModelSerializer):
 
-    primary_category = PrimaryCategorySerializer()
-    secondary_category = SecondaryCategorySerializer()
-    size = SizeSerializer(read_only=True, many=True)
-    tags = TagSerializer(read_only=True, many=True)
+    # primary_category = PrimaryCategorySerializer()
+    # secondary_category = SecondaryCategorySerializer()
+    # size = SizeSerializer(read_only=True, many=True)
+    # tags = TagSerializer(read_only=True, many=True)
 
     class Meta:
         model = Product
-        fields = ['id', 'name', 'price', 'primary_category',
-                  'secondary_category', 'size', 'tags']
+        fields = ['id', 'name', 'price']
--- hellowardrobe/product/views.py
+++ hellowardrobe/product/views.py
@@ -1,15 +1,26 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .models import Product, Tag, Size
-from api.serializers.product_serializer import ProductSerializer, TagSerializer, SizeSerializer
+from rest_framework.pagination import PageNumberPagination
+from .models import Product, Tag, Size, PrimaryCategory, SecondaryCategory
+from api.serializers.product_serializer import ProductSerializer, TagSerializer, SizeSerializer, PrimaryCategorySerializer, SecondaryCategorySerializer
 
 
 # Create your views here.
 
 @api_view(['GET'])
 def list_products(request):
+    paginator = PageNumberPagination()
+    paginator.page_size = 30
+
     products = Product.objects.select_related('primary_category', 'secondary_category').prefetch_related(
-        'size', 'tags').filter(is_active=True).defer('created_on', 'updated_on', 'is_active')
+        'size', 'tags').filter(is_active=True)
+
+    if request.GET.getlist('primary'):
+        products = products.filter(
+            primary_category__name__in=request.GET.getlist('primary'))
+    if request.GET.getlist('secondary'):
+        products = products.filter(
+            secondary_category__name__in=request.GET.getlist('secondary'))
     if request.GET.getlist('size'):
         products = products.filter(size__name__in=request.GET.getlist('size'))
     if request.GET.getlist('occasion'):
@@ -19,17 +30,32 @@ def list_products(request):
         products = products.filter(tags__name__in=request.GET.getlist('color'))
     if request.GET.getlist('type'):
         products = products.filter(tags__name__in=request.GET.getlist('type'))
-    serializer = ProductSerializer(products.distinct(), many=True)
-    return Response(serializer.data)
+    if request.GET.getlist('sort'):
+        products = products.order_by(request.GET.get('sort'))
+    products = products.only(
+        'name', 'price', 'primary_category', 'secondary_category').distinct()
+    result = paginator.paginate_queryset(products, request)
+    serializer = ProductSerializer(result, many=True)
+    return paginator.get_paginated_response(serializer.data)
 
 
 @api_view(['GET'])
 def filter_details(request):
+    primary_category_data = PrimaryCategory.objects.filter(is_active=True)
+    secondary_category_data = SecondaryCategory.objects.filter(is_active=True)
     filter_data = Tag.objects.filter(is_active=True)
     size_data = Size.objects.filter(is_active=True)
+
+    primary_category_serializer = PrimaryCategorySerializer(
+        primary_category_data, many=True)
+    secondary_category_serializer = SecondaryCategorySerializer(
+        secondary_category_data, many=True)
     filter_data_serializer = TagSerializer(filter_data, many=True)
     size_data_serializer = SizeSerializer(size_data, many=True)
+
     response = {
+        'primary_category_details': primary_category_serializer.data,
+        'secondary_category_details': secondary_category_serializer.data,
         'filter_details': filter_data_serializer.data,
         'size_details': size_data_serializer.data
     }
